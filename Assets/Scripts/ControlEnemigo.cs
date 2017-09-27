e837d958910c81600b848f3002aa2083967557e2
adryfab <adry_fab@hotmail.com>
2017-09-26 19:00:19 -0500

    Primera Subida
    Primera Subida

---
+++ Assets/Scripts/ControlEnemigo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEnemigo : MonoBehaviour
+{
+    public float vel = -1f;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+    void FixedUpdate()
+    {
+        Vector2 v = new Vector2(vel, 0);
+        rb.velocity = v;
+    }
+}
