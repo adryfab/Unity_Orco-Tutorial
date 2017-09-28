4eb9756f9c3d5fb5af8b7c34289f26ee815b172e
adryfab <adry_fab@hotmail.com>
2017-09-27 20:25:04 -0500

    Inicio creacion Bala
    Fin de video de semana 4 - inicio de creación de una bala

---
+++ Assets/Scripts/ControlBala.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBala : MonoBehaviour
+{
+    public float speed = 6;
+    public float lifeTime = 2;
+    public Vector3 direction = new Vector3(-1,0,0);
+
+    Vector3 stepVector;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(gameObject, lifeTime);
+        rb = GetComponent<Rigidbody2D>();
+        stepVector = speed * direction.normalized;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        rb.velocity = stepVector;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
